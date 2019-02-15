24956788e808f8eda5902e2b19b9a4143cb7bdcb
Aragon <aragonwa@gmail.com>
2019-02-15 11:55:24 -0800

    Adding javascript compliation to build tool

--- public/index.js
+++ public/index.js
@@ -1,14 +1,1 @@
-(function() {
-  M.AutoInit();
-
-  var searchIcon = document.querySelectorAll('.hw-nav-search .material-icons')[0];
-
-  var searchForm = document.querySelectorAll('.hw-nav-search form input')[0];
-
-  searchForm.onfocus = function(e){
-    searchIcon.innerHTML = "";
-  }
-  searchForm.onblur = function(e){
-    searchIcon.innerHTML = "search";
-  }
-})();
+(function(){M.AutoInit()})();(function(){var searchIcon=document.querySelectorAll(".hw-nav-search .material-icons")[0];var searchForm=document.querySelectorAll(".hw-nav-search form input")[0];searchForm.onfocus=function(e){searchIcon.innerHTML=""};searchForm.onblur=function(e){searchIcon.innerHTML="search"}})();
