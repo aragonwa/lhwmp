77b5a6d41b03238d548438875cf41f09952ec754
Aragon <aragonwa@gmail.com>
2019-03-05 12:37:03 -0800

    carousel init

--- src/scripts/MD-init.js
+++ src/scripts/MD-init.js
@@ -1,4 +1,18 @@
 /** Initiate Materialize scripts */
 (function() {
   M.AutoInit();
+``  var elems = document.querySelectorAll('.carousel');
+  document.addEventListener('DOMContentLoaded', function() {
+    var instances = M.Carousel.init(elems, { fullWidth: true });
+  });
+  var next = document.querySelectorAll('#next');
+  next[0].addEventListener('click', function() {
+    var instance = M.Carousel.getInstance(elems[0]);
+    instance.next();
+  });
+  var prev = document.querySelectorAll('#prev');
+  prev[0].addEventListener('click', function() {
+    var instance = M.Carousel.getInstance(elems[0]);
+    instance.prev();
+  });
 })();
